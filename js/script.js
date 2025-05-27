62d42268d99403dfedcc8e9871e9a4f1ef378191
minjungH <lunahongmj@gmail.com>
2025-05-27 13:17:30 +0900

    이름수정

--- js/script.js
+++ js/script.js
@@ -1,29 +1,31 @@
-// Isotope 인스턴스 객체 생성
-const sectionIso = new Isotope(".section", {
-  // options
-  itemSelector: "article",
-  masonry: {
-    // use element for option
-    columnWidth: "article",
-  },
-});
+window.addEventListener("load", () => {
+  // Isotope 인스턴스 객체 생성
+  const sectionIso = new Isotope(".section", {
+    // options
+    itemSelector: "article",
+    masonry: {
+      // use element for option
+      columnWidth: "article",
+    },
+  });
 
-const sortingBtns = document.querySelectorAll(".sorting li");
-sortingBtns.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    e.preventDefault();
+  const sortingBtns = document.querySelectorAll(".sorting > li");
+  sortingBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
 
-    // 재정렬
-    const filter = e.currentTarget.querySelector("a").getAttribute("href");
-    sectionIso.arrange({
-      filter: filter,
+      // 재정렬
+      const filter = e.currentTarget.querySelector("a").getAttribute("href");
+      sectionIso.arrange({
+        filter: filter,
+      });
+
+      // on클래스 모두 제거후 클릭한곳에 on클래스 추가
+      sortingBtns.forEach((btnEl) => {
+        btnEl.classList.remove("on");
+      });
+      e.currentTarget.classList.add("on");
     });
-
-    sortingBtns.forEach((btnEl) => {
-      btnEl.classList.remove("on");
-    });
-
-    e.currentTarget.classList.add("on");
   });
 });
 
@@ -34,7 +36,6 @@ const modalClose = document.querySelector(".modal-close");
 window.addEventListener("load", () => {
   modal.classList.add("active");
 });
-
 modalClose.addEventListener("click", () => {
   modal.classList.remove("active");
 });
